8b7e794dd3ae86e480dd049029686d4f9ca19c9e
smc <smc@oceansoft.com.cn>
2016-07-11 08:48:44 +0800

    初始化

---
+++ src/main/webapp/resources/my/common.js
@@ -0,0 +1,13 @@
+(function($){
+    $.add=function(x,y){
+        return x+y;
+    }
+
+    $.test = function (obj) {
+        obj.name;
+        obj.success(4);
+        alert("name: "+ name);
+    }
+
+
+})(smc)
